b85c1b28879e570fd4a1913e0379674b5a1f456a
Ivan <80452629+ivanzmushko@users.noreply.github.com>
2022-02-24 21:25:04 +0300

    fiat: add script.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function toReadable (number) {
-  
+  const numbers = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+    const second_numbers = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+    let neededDigit = number % 10;
+    
+    if (number < 20) { 
+        return numbers[number];
+    }
+    else if (number < 100) {
+        return second_numbers[~~(number/10)-2] + (neededDigit? " " + numbers[neededDigit]: "");
+    }
+    else if (number < 1000) {
+        return numbers[~~(number/100)] + " hundred" + (number%100 == 0? "" : " " + toReadable(number%100));
+    }
+    else {
+        return toReadable(~~(number/1000)) + " thousand" + (number%1000 != 0? " " + toReadable(number%1000): "");
+    }
 }
